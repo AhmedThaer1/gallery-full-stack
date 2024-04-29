a68a77340d26e7f785ce8f36941b41cd8399467d
AhmedThaer1 <110353919+AhmedThaer1@users.noreply.github.com>
2024-04-29 15:54:18 +0300

    adding styling and loading spiner for uploading state

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -30,7 +30,7 @@ const useUploadThingInputProps = (...args: Input) => {
   };
 };
 
-function ButtionSvg() {
+function UploadSvg() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -49,14 +49,43 @@ function ButtionSvg() {
   );
 }
 
+function LoadingSpinerSvg() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="white"
+    >
+      <path
+        d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+        opacity=".25"
+      />
+      <path
+        d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+        className="spinner_ajPY"
+      />
+    </svg>
+  );
+}
+
 export function SimpleUploadButton() {
   const router = useRouter();
+
   const { inputProps } = useUploadThingInputProps("imageUploader", {
     onUploadBegin() {
-      toast("Uploading image...", {
-        duration: 100000,
-        id: "upload-begin",
-      });
+      toast(
+        <div className="flex flex-row items-center gap-4 text-white">
+          <LoadingSpinerSvg />{" "}
+          <span className="text-lg">Uploading image...</span>
+        </div>,
+        {
+          duration: 100000,
+          id: "upload-begin",
+          className: "bg-background text-foreground border-border shadow-lg",
+        },
+      );
     },
     onClientUploadComplete() {
       toast.dismiss("upload-begin");
@@ -68,7 +97,7 @@ export function SimpleUploadButton() {
   return (
     <div>
       <label htmlFor="upload-input" className="cursor-pointer">
-        <ButtionSvg />
+        <UploadSvg />
       </label>
       <input
         id="upload-input"
